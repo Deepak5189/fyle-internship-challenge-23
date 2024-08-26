5ba1b47ad529cdc57e1c6a7aa4ff1fa995382a82
Deepak <deepaks.rawat144@gmail.com>
2024-08-26 23:35:12 +0530

    production ready

--- src/app/environment.prod.ts
+++ src/app/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment={
   production:true,
   // token: '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',
-  token: process.env['token'] || ''
+  token: ''
 }
